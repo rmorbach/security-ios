f45f2c360bcf75afd59f7be2feab9f7d846e5ec9
Rodrigo Morbach <morbachrodrigo@gmail.com>
2018-11-11 22:24:43 -0200

    Initial version

---
+++ Security.playground/Contents.swift
@@ -0,0 +1,45 @@
+/**
+ - Author:
+ Rodrigo Morbach
+ */
+
+import UIKit
+
+let b64StringKey = """
+MIIEowIBAAKCAQEAo81+4NLKXyrWs9k853t+9kK0u0i8owHO7FRd1SJXCyFDRpcy823YHRydxn84NLkLVOASGiILJb5FpeZ3HzNhzohDy//MJcTwp00KQSiXPgOtMoZbEBzIu7RlSWyroxHTwIbyTAPaGaqV8j3EGYWahfubUlHL/lPMJk/eEHj3E/+1+oGaJ5IpIEG9HzrjcEyYjXAPyfc9jWFFMf4V37GaD8+krbp+pIcNCXgViaIXI4l9MuQjAp1OYbKA32h+UhTSXZsmp0ZWlGy/LhSK3VLt5cNUzXYHpPQyke1kb1vyLcZLAbKlqfrz4k84YOdZNJdp399x79B+S2o/Z6+1IjAwIwIDAQABAoIBAA29zTfBumqkad5f/ZBMl5726dMb6+Z5RHYmZIUr4p7gG5UEiMIyKolt33CJ0KaS1XgGOigi4TBTF32ugu5e1zgsmnkDjKLbtCkDOTaBJLr2ADKD+4fmmkWUGyElpcdCCIBKWD5EEajKJ9a3RsgYIX/wiejfss8zRKX8JXY+559jCs23Vyrtd0sTOhZ5FPX2N/SiMvUuNviDLOUGZ00xOdkz9gI5Aorlc7VFwR/Ef3Gz95Vw9ewLl5QuoGUZ0X3CCyj6tTOewzkpUYRtDAoLHmB2nJpEop9yK7FFb7U4CNOJuTXV/YyUIBR+BaPmV5skUvH/MFkRUPzak+eHaULXfmkCgYEA1yHQdA7l1mexjrqj9aMKnQBfyzmPV7mcLBftweh+S9RC10U/wOEaQMD5wXL6eJhvdK+Vcd2zvOeHDqtNbG2aSPeUZpGVPYisA0USg4rJgaWa5wdA1uWZAysBhD7rsr6QXGAtJGcoV4Z9TLK5UKMxWB2ezgnAlXXmW7I+SX/TVTkCgYEAwut1pqxEiF+0pYjGd+k0uaqRcHXPai1jVWRReS8Ob1emhfsbMMKjaWtCY0WQ8nbYnf/HNWlTuze7qH2Gqfbm8JKBS9yR1oGExonbjOTmppvxo7IGh6Kmd4DVHdmvPbevHFgN0vsfBK/7opbNHdH2PmOBCPwdvHhQ2n0qfuxD7DsCgYBRg354eyybEK82ZYG6kQx0cK/bUPP/SVTjYC22S2GPWd35s0N00dYgsv3vTqGZECkrDVySyG5SJWgHJuEvNFhBTY3tTzw9FIwYsRNSxEOV3CnVX+oxgs13ZdQtR6oHEC2i0s5H/M5MW6keZpnQ9AS+XnrNluRhvUqLc/4l20k4AQKBgH0kZtinaSiVKYnNkn6R/wPk+pNVT+E/WrMMQTKof4AEqikC2qYnkHqyHmFV3IWOv+xQtzkXcmaUlarLTJX6qrJxnQlzhz5M0U4WXBrNKzh0jmMmfQK/7lhzImCTzeq173snHVkDWRrNUaVFZFNs5ceQYtu94emK5X4hNhNe5QzZAoGBAJfQcjsgTQnUtNAc89xTpDmlMaafAy8QIZC0JDosmRYB1TQt3jUBK+Q9L2frO1bDmJ9wR2ssnrMQMTRSFp7UV+LGG4R5U2AkfBWOUceCPWaRjYgkOwzAOkHJ2Byg0JetmkCIFeri2o5rrfbMJWIpD+XjCN0ZI1vy9enMEPAoA0lp
+"""
+
+let privateKey = RSA.createPrivateKeyFromBase64(b64StringKey)
+
+if let generatedKeyData = RSA.generateSecureRandomData() {
+    print(generatedKeyData.base64EncodedString())
+}
+
+// RSA.createAssimetricKeyPair()
+
+if let publicKey = RSA.calculatePublicKey(from: privateKey!) {
+    if let encryptedData = RSA.encrypt(data: "Data to be encrypted", with: publicKey) {
+        if let result = RSA.decrypt(data: encryptedData, with: privateKey!) {
+            debugPrint(String(data: result, encoding: String.Encoding.utf8)!)
+        }
+    }
+} else {
+    debugPrint("Failed calculating the public key from private one")
+}
+
+if let publicKey = RSA.calculatePublicKey(from: privateKey!) {
+    
+    let encryptedBytes = RSA.secKeyEncrypt("Data to be encrypted", publicKey: publicKey)
+    print("Encrypted String: \(encryptedBytes.base64EncodedString())")
+    
+    if let result = RSA.secKeyDecrypt(encryptedBytes, privateKey: privateKey!) {
+        print(result)
+    }
+}
+
+/// Sign data using the private key
+let dataToBeSigned = "Data to be signed".data(using: .utf8)
+if let result = RSA.signData(dataToBeSigned!, with: privateKey!) {
+    print(result)
+}
+
---
+++ Security.playground/Sources/RSA.swift
@@ -0,0 +1,286 @@
+import Foundation
+import Security
+
+// Methods are based on https://developer.apple.com/library/archive/documentation/Security/Conceptual/cryptoservices/Introduction/Introduction.html#//apple_ref/doc/uid/TP40011172
+public struct RSA {
+    
+    /**
+     
+     Many encryption algorithms rely on cryptographically strong pseudorandom numbers.
+     - returns:
+     Generate random data
+     - Reference:
+     [SecRandomCopyBytes](https://developer.apple.com/documentation/security/1399291-secrandomcopybytes)
+     
+     */
+    public static func generateSecureRandomData() -> Data? {
+        
+        var bts = [UInt8](repeating: 0, count: 16)
+        let status = SecRandomCopyBytes(kSecRandomDefault, bts.count, &bts)
+        if status == errSecSuccess {
+            print(bts)
+            let dt = Data(bytes: bts)
+            return dt
+        }
+        return nil
+    }
+    
+    
+    /**
+     
+     Create an external representation of a key for transmission.
+     
+     - parameters:
+        - key: Public or Private key
+     - returns:
+     Base64 representaion of the given key
+     - Reference:
+     [Storing Keys as Data](https://developer.apple.com/documentation/security/certificate_key_and_trust_services/keys/storing_keys_as_data)
+     */
+    private static func convertKeyToBase64String(key: SecKey) -> String? {
+        
+        var error: Unmanaged<CFError>?
+        if let cfData = SecKeyCopyExternalRepresentation(key, &error) {
+            let data: Data = cfData as Data
+            let b64Key = data.base64EncodedString()
+            return b64Key
+        }
+        
+        return nil
+    }
+    
+    /**
+     Create an assimetric ley pair, but does not store the private one in the keychain.
+     
+     - returns:
+     Generated Private Key
+     - Reference:
+     [Generate Keys](https://developer.apple.com/documentation/security/certificate_key_and_trust_services/keys/generating_new_cryptographic_keys#2863927)
+     */
+    public static func createAssimetricKeyPair() -> SecKey? {
+        
+        let attributes: [String: Any] = [kSecAttrKeyType as String: kSecAttrKeyTypeRSA as String,
+                                         kSecAttrKeySizeInBits as String: 2048,
+                                         kSecPrivateKeyAttrs as String:
+                                            [kSecAttrIsPermanent as String: false,
+                                             kSecAttrCanEncrypt as String: true,
+                                             kSecAttrCanDecrypt as String: true,
+                                             kSecAttrCanSign as String: true
+            ]];
+        
+        var error: Unmanaged<CFError>?
+        guard let privateKey = SecKeyCreateRandomKey(attributes as CFDictionary, &error) else {
+            let err = error!.takeRetainedValue() as Error
+            debugPrint(err)
+            return nil
+        }
+        
+        return privateKey
+    }
+    
+    /**
+     
+     Restores a key from an external representation of that key.
+     
+     - parameters:
+        - base64String: Base64 encoded representation of a private key
+     - returns:
+     Private key
+     - Reference:
+     [SecKeyCreateWithData](https://developer.apple.com/documentation/security/1643701-seckeycreatewithdata)
+     */
+    public static func createPrivateKeyFromBase64(_ base64String: String) -> SecKey? {
+        
+        let attributes: [String: Any] = [
+            kSecAttrType as String: kSecAttrKeyTypeRSA,
+            kSecAttrKeyClass as String: kSecAttrKeyClassPrivate,
+            kSecAttrKeySizeInBits as String: 2048
+        ];
+        
+        if let cfData = Data(base64Encoded: base64String.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)) {
+            var error: Unmanaged<CFError>?
+            guard let generatedKey = SecKeyCreateWithData(cfData as CFData, attributes as CFDictionary, &error) else {
+                let err = error!.takeRetainedValue() as Error
+                debugPrint(err)
+                return nil
+            }
+            return generatedKey
+        } else {
+            debugPrint("Could not convert base64 encodedString to Data")
+        }
+        
+        return nil
+    }
+    
+    /**
+     
+     Encrypts a block of plaintext.
+     
+     - parameters:
+        - data: Raw data to be encrypted
+        - publicKey: Public key encrypt the data
+     - returns:
+     Encrypt data
+     - Reference:
+     [SecKeyEncrypt](https://developer.apple.com/documentation/security/1617956-seckeyencrypt)
+     */
+    public static func secKeyEncrypt(_ data: String, publicKey: SecKey) -> Data {
+        
+        var cipherLen = SecKeyGetBlockSize(publicKey)
+        var cipherText = [UInt8](repeating: 0, count: cipherLen)
+        let plainText = [UInt8](data.utf8)
+        let plainTextDataLength = Int(plainText.count)
+        
+        let status = SecKeyEncrypt(publicKey, SecPadding.init(rawValue: 0), plainText, plainTextDataLength, &cipherText, &cipherLen)
+        
+        if status != errSecSuccess {
+            debugPrint("Failed encrypting data \(status)")
+        }
+        
+        return Data(bytes: cipherText)
+    }
+    
+    /**
+     
+     Decrypts a block of ciphertext
+     
+     - parameters:
+        - data: Raw data to be decrypted
+        - privateKey: Private key decrypt the data
+     - returns:
+     Plain text     
+     - Reference:
+     [SecKeyDecrypt](https://developer.apple.com/documentation/security/1617894-seckeydecrypt)
+     */
+    public static func secKeyDecrypt(_ data: Data, privateKey: SecKey) -> String? {
+        
+        var plainTextLen = SecKeyGetBlockSize(privateKey)
+        var plainText = [UInt8](repeating: 0, count: plainTextLen)
+        let dataBytes = [UInt8](data)
+        
+        let status = SecKeyDecrypt(privateKey, SecPadding.init(rawValue: 0), dataBytes, dataBytes.count, &plainText, &plainTextLen)
+        
+        if status == errSecSuccess {
+            return String(bytes: plainText, encoding: String.Encoding.utf8)
+        } else {
+            debugPrint("Failed decrypting data \(status)")
+            return nil
+        }
+    }
+    /**
+     
+     Encrypt data with a given key.
+     - parameters:
+        - data: Raw data to be encrypted
+        - with: Public key encrypt the data
+     - returns:
+     Encrypted data
+     - Reference:
+     [SecKeyCreateEncryptedData](https://developer.apple.com/documentation/security/certificate_key_and_trust_services/keys/storing_keys_as_data)
+     */
+    public static func encrypt(data toBeEncrypted: String, with publicKey: SecKey) -> Data? {
+        
+        let size = SecKeyGetBlockSize(publicKey)
+        debugPrint(size)
+        
+        let algorithm = SecKeyAlgorithm.rsaEncryptionPKCS1
+        
+        guard SecKeyIsAlgorithmSupported(publicKey, SecKeyOperationType.encrypt, algorithm) else {
+            debugPrint("Algorithm not supported")
+            return nil
+        }
+        
+        guard let dt = toBeEncrypted.data(using: .utf8) else {
+            debugPrint("Could not create data")
+            return nil
+        }
+        
+        var error: Unmanaged<CFError>?
+        //This function first create a symmetric key using AES, then encrypt the data using this key, then encrypt the key using RSA public key provided, and, at last, and assemble a block of data composed by (RSA encrypted session key (AES) + the AES encrypted data + 16-byte AES-GCM tag)
+        guard let result = SecKeyCreateEncryptedData(publicKey, algorithm, dt as CFData, &error) else {
+            let err = error!.takeRetainedValue() as Error
+            debugPrint(err)
+            return nil
+        }
+        
+        let encryptedData = result as Data
+        return encryptedData
+    }
+    
+    /**
+     
+     Decrypt data with a given key.
+     - parameters:
+        - data: Encrypted data generated from SecKeyCreateEncryptedData
+        - with: Private key to decrypt the data
+     - returns:
+     Decrypted data
+     - Reference:
+     [SecKeyCreateDecryptedData](https://developer.apple.com/documentation/security/1644043-seckeycreatedecrypteddata)
+     */
+    public static func decrypt(data encryptedData: Data, with privateKey: SecKey) -> Data? {
+        
+        let algorithm = SecKeyAlgorithm.rsaEncryptionPKCS1
+        
+        guard SecKeyIsAlgorithmSupported(privateKey, SecKeyOperationType.decrypt, algorithm) else {
+            debugPrint("Algorithm not supported")
+            return nil
+        }
+        
+        var error: Unmanaged<CFError>?
+        guard let result = SecKeyCreateDecryptedData(privateKey, algorithm, encryptedData as CFData, &error) else {
+            let err = error!.takeRetainedValue() as Error
+            debugPrint(err)
+            return nil
+        }
+        
+        let dt = result as Data
+        return dt
+    }
+    
+    /**
+     
+     Creates the cryptographic signature for a block of data using a private key and specified algorithm.
+     - parameters:
+        - data: Block of data to be signed
+        - with: Private key to sign the block
+     - returns:
+     Base 64 encoded string of the signature.
+     - Reference:
+     [SecKeyCreateSignature](https://developer.apple.com/documentation/security/1643916-seckeycreatesignature)
+     */
+    public static func signData(_ data: Data, with privateKey: SecKey) -> String? {
+        
+        let algorithm = SecKeyAlgorithm.rsaSignatureDigestPKCS1v15SHA1
+        
+        guard SecKeyIsAlgorithmSupported(privateKey, SecKeyOperationType.sign, algorithm) else {
+            debugPrint("Algorithm not supported")
+            return nil
+        }
+        
+        var error: Unmanaged<CFError>?
+        guard let result = SecKeyCreateSignature(privateKey, algorithm, data as CFData, &error) else {
+            let err = error!.takeRetainedValue() as Error
+            debugPrint(err)
+            return nil
+        }
+        
+        let dt = result as Data
+        return dt.base64EncodedString()
+    }
+    
+    /**
+     
+     Gets the public key associated with the given private key.
+     - parameters:
+        - from: Private key to calculate the public one
+     - returns:
+     The public key corresponding to the given private key.
+     - Reference:
+     [SecKeyCopyPublicKey](https://developer.apple.com/documentation/security/1643774-seckeycopypublickey)
+     */
+    public static func calculatePublicKey(from privateKey: SecKey) -> SecKey? {
+        return SecKeyCopyPublicKey(privateKey)
+    }
+
+}
